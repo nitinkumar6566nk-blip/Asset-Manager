1a42e3beed6e40045181a586f147d1e2bc719ed9
nitinkumar6566nk-blip <nitinkumar6566nk@gmail.com>
2026-07-31 17:00:14 +0000

    Fix TypeScript errors in app.ts for Vercel

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import pinoHttp from "pino-http";
+const pinoHttp = require("pino-http");
 import router from "./routes";
 import { logger } from "./lib/logger";
 import { authMiddleware } from "./middlewares/authMiddleware";
@@ -12,14 +12,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
